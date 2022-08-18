588de09727733fafdeddad22039c44cd828faceb
fredyvaron <fvaron@itfip.edu.co>
2022-08-18 08:16:33 -0500

    add send message

--- src/Component/Contacteme.jsx
+++ src/Component/Contacteme.jsx
@@ -1,31 +1,105 @@
-import React from 'react'
-
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import Swal from 'sweetalert2'
 function Contacteme() {
+  const form = useRef();
+console.log( import.meta.env.VITE_APP_USER_ID)
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_APP_SERVICE_ID,
+        import.meta.env.VITE_APP_TEMPLATE_ID,
+        form.current,
+        import.meta.env.VITE_APP_USER_ID
+      )
+      .then(function(response) {
+        Swal.fire(
+          'Se Envio El Mensaje!',
+          'Pronto estaremos en contacto!',
+          'success'
+        )
+        console.log('SUCCESS!', response.status, response.text);
+     }, function(error) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Ocurrio un error',
+        
+      })
+        console.log('FAILED...', error);
+     });
+     e.target.reset()
+  };
   return (
     <div name="contactame">
       <section className="bg-white dark:bg-gray-900">
-  <div className="py-8 px-4 mx-auto max-w-screen-md lg:py-16 lg:px-6" >
-      <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contactame</h2>
+        <div className="py-8 px-4 mx-auto max-w-screen-md lg:py-16 lg:px-6">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+            Contactame
+          </h2>
 
-      <form action="#" className="space-y-8">
-          <div>
-              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
-              <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="nombre@email.com" required/>
-          </div>
-          <div>
-              <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Tema</label>
-              <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Escriba el tema" required/>
-          </div>
-          <div className="sm:col-span-2">
-              <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Mensaje</label>
-              <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Deje un Comentario..."></textarea>
-          </div>
-          <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Enviar Mensaje</button>
-      </form>
-  </div>
-</section>
+          <form ref={form} onSubmit={sendEmail} className="space-y-8">
+            <div>
+              <label
+                htmlFor="subject"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Nombre
+              </label>
+              <input
+                type="text"
+                id="subject"
+                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="Escriba el Nombre"
+                name="user_name"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="nombre@email.com"
+                name="user_email"
+                required
+              />
+            </div>
+
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="message"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+              >
+                Mensaje
+              </label>
+              <textarea
+                id="message"
+                rows="6"
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Deje un Comentario..."
+                name="message"
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Enviar Mensaje
+            </button>
+          </form>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default Contacteme
+export default Contacteme;
--- src/Component/Foote.jsx
+++ src/Component/Foote.jsx
@@ -7,18 +7,18 @@ import Cv from "../assets/Footer/CV-FredyAlbertoVaron-FullStackDeveloper (1).pdf
 function Foote() {
   return (
     <div>
-      <footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="/" class="hover:underline">Fredy Alberto™</a>
+      <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="/" className="hover:underline">Fredy Alberto™</a>
     </span>
-    <ul class="flex flex-wrap items-end mt-3 space-x-6  text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+    <ul className="flex flex-wrap items-end mt-3 space-x-6  text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-            <a href="https://www.linkedin.com/in/fredyalbertovaronguzman/" target={'_blank'}class="mr-8 hover:underline md:mr-6 "> <img className='w-8 h-8 rounded-md hover:bg-blue-300' src={Linkedinsvg} draggable="false"/></a>
+            <a href="https://www.linkedin.com/in/fredyalbertovaronguzman/" target={'_blank'}className="mr-8 hover:underline md:mr-6 "> <img className='w-8 h-8 rounded-md hover:bg-blue-300' src={Linkedinsvg} draggable="false"/></a>
         </li>
         <li>
-            <a href="https://github.com/fredyvaron" target={'_blank'}class="mr-4 hover:underline md:mr-6"><img className='w-8 h-8 rounded-full hover:bg-blue-300' src={Githubsvg} draggable="false"/></a>
+            <a href="https://github.com/fredyvaron" target={'_blank'}className="mr-4 hover:underline md:mr-6"><img className='w-8 h-8 rounded-full hover:bg-blue-300' src={Githubsvg} draggable="false"/></a>
         </li>
         <li>
-            <a href={Cv} target={'_blank'}class="mr-4 hover:underline md:mr-6"><img className='w-8 h-8 rounded-full hover:bg-blue-300' src={Curriculosvg} draggable="false"/></a>
+            <a href={Cv} target={'_blank'}className="mr-4 hover:underline md:mr-6"><img className='w-8 h-8 rounded-full hover:bg-blue-300' src={Curriculosvg} draggable="false"/></a>
         </li>
 
     </ul>
